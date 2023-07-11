6efbf6b8fffc954e9c064712bd1578eba16f362b
Ronald Mjonono <mjay@M-JAY.local>
2023-07-11 20:49:12 +0200

    add es6_modules

---
+++ modules/Storage.js
@@ -0,0 +1,25 @@
+export const getBook=()=> {
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+
+  return books;
+}
+
+export const addBook=(newBook)=>{
+  const books = getBook();
+  books.push(newBook);
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+export const removeBook =(title)=>{
+  const books = getBook();
+  const index = books.findIndex((book) => book.title === title);
+  if (index !== -1) {
+    books.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
